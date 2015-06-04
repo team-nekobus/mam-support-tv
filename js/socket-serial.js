6c0afc2ca3357d47ea2d432416536e990b86c913
flatbird <flatbird.hiratori@gmail.com>
2015-06-04 21:25:18 +0900

    construct the line becouse it's fragmented in the real STB.

--- js/socket-serial.js
+++ js/socket-serial.js
@@ -8,28 +8,36 @@
   };
 
   var SocketSerial = {
+    _socket: undefined,
+    _buffer: '',
+    _onopen: function () {},
+    _ondata: function () {},
+    _onerror: function () {},
+
   	open: function() {
       var socket = navigator.mozTCPSocket.open(ADDRESS, PORT);
-      this.socket = socket;
+      this._socket = socket;
       var self = this;
       socket.onopen = function () {
         console.log('Opened');
         if (!window.navigator.tv) {
           socket.send(JSON.stringify(gSerialConfig));
         }
-        if (self.onopen) {
-          self.onopen();
-        }
+        self._onopen();
       }
       socket.ondata = function (evt) {
         if (typeof evt.data === 'string') {
-          var line = evt.data;
-          if (line[line.length-1] === '\n') {
-            line = line.slice(0, line.length-1);
-          }
-          console.log(line);
-          if (self.ondata) {
-            self.ondata(line);
+          var data = evt.data;
+          // console.log(data);
+          if (data[data.length-1] === '\n') {
+            var line = self._buffer + data.slice(0, data.length-1);
+            self._buffer = '';
+            console.log('line ' + line);
+            if (line[0] === ':') {
+              self._ondata(line);
+            }
+          } else {
+            self._buffer += data;
           }
         } else {
           console.error('Received a Uint8Array');
@@ -37,22 +45,20 @@
       }
       socket.onerror = function (evt) {
         console.error('Error:' + evt.type);
-        if (self.onerror) {
-          self.onerror(evt);
-        }
+        self._onerror(evt);
       }
     },
     send: function(data) {
-      this.socket.send(data);
+      this._socket.send(data);
     },
     setOpenHandler: function(onopen) {
-      this.onopen = onopen;
+      this._onopen = onopen;
     },
     setDataHandler: function(ondata) {
-      this.ondata = ondata;
+      this._ondata = ondata;
     },
     setErrorHandler: function(onerror) {
-      this.onerror = onerror;
+      this._onerror = onerror;
     }
   };
 
